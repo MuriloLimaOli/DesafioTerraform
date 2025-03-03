a3ac457b3746d8cb28d7de85aa37ee5b5a4d216b
Murilo Lima de Oliveira <murilo.oliveiralima25@gmail.com>
2025-03-03 17:30:51 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -98,6 +98,7 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   from_port         = 22
   to_port           = 22
   ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
   cidr_ipv6         = "::/0"
 }
 
@@ -108,6 +109,7 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
   from_port         = 80
   to_port           = 80
   ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
   cidr_ipv6         = "::/0"
 }
 
@@ -118,6 +120,7 @@ resource "aws_vpc_security_group_egress_rule" "outbound" {
   from_port         = 0
   to_port           = 0
   ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
   cidr_ipv6         = "::/0"
 }
 
